cb29efb7e0234976193615cc37189cdbcf7f48d1
mohnark <kumail0691@gmail.com>
2025-04-22 15:02:43 +0300

    Basepath added for files

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const BASEURL='tartu-cell-towers-viz/'
-export const TARTU_CELL_TOWERS='https://kodu.ut.ee/~raza/tartu_city_celltowers_edu.geojson'
-export const TARTU_CITY_DISTRICTS = 'https://kodu.ut.ee/~raza/tartu_city_districts_edu.geojson'
+export const TARTU_CELL_TOWERS='tartu_city_celltowers_edu.geojson'
+export const TARTU_CITY_DISTRICTS = 'tartu_city_districts_edu.geojson'
