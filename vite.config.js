30373610bd9d68ab7d5285e0552a4930e998a30c
mohnark <kumail0691@gmail.com>
2025-04-22 14:46:55 +0300

    basename fixed

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/tartu-cell-towers-viz/',
   plugins: [react()],
   optimizeDeps: {
     include: [
